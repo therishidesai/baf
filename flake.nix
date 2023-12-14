280c2b1d58f000bff982dcee8fbdb013b0c8548f
Rishi Desai <rishi@saronic.com>
2023-12-14 11:56:12 -0600

    fix: remove hard coded evalSystem

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
             hixProject =
               final.haskell-nix.hix.project {
                 src = ./.;
-                evalSystem = "x86_64-linux";
               };
           })
         ];
